a119c48239413b4f1cc5cbb479afb7534788fc16
bad4iz <bad4iz@gmail.com>
2017-01-25 23:03:33 +0400

    56

--- module/BookList/config/module.config.php
+++ module/BookList/config/module.config.php
@@ -5,4 +5,28 @@
  * Date: 25.01.2017
  * Time: 20:52
  */
-return array();
+return array(
+    'controllets'=>array(
+        'inbokables'=>array(
+            'BookList\Controller\Book'=>'BookList\Controller\BookController',
+        ),
+    ),
+    'router'=>array(
+        'routes'=>array(
+            'boook'=>array(
+                'type'=>'segment',
+                'options'=>array(
+                    'route'=>'/book[/][:action][/:id]',
+                    'comstraints'=>array(
+                        'action'=>'[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'=>'[0-9]+',
+                    ),
+                    'defaults'=>array(
+                        'controller'=>'BookList\Controller\Book',
+                        'action'=>'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
