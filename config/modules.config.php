a08b146a83f2c282d0cb18caf877bbc9faf8770a
bad4iz <bad4iz@gmail.com>
2017-01-25 22:11:17 +0400

    10

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,6 +11,8 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Serializer',
+    'Zend\Log',
     'Zend\Form',
     'Zend\Router',
     'Zend\Validator',
