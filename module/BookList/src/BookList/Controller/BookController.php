2e0e629bcef615eb44e81950495cae75bfab3354
bad4iz <bad4iz@gmail.com>
2017-01-26 21:28:15 +0400

    add

--- module/BookList/src/BookList/Controller/BookController.php
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -9,14 +9,26 @@
 namespace BookList\Controller;
 
 
+use BookList\Form\BookForm;
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class BookController extends AbstractActionController{
     public function indexAction()
     {
-        return [];
+        return new ViewModel(array(
+            'book'=>[]
+        ));
     }
     public function addAction(){
+        $form = new BookForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if($request->isPost()){
+
+        }
+        return array('form'=> $form);
 
     }
     public function editAction(){
