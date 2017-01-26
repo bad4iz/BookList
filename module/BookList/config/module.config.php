9398930bd4381d4f472e151f133273b4e96f672d
bad4iz <bad4iz@gmail.com>
2017-01-26 20:11:17 +0400

    bookList

--- module/BookList/config/module.config.php
+++ module/BookList/config/module.config.php
@@ -35,7 +35,44 @@ return array(
     ),
     'view_manager' => [
         'template_path_stack' => [
-            'ZendSkeletonModule' => __DIR__ . '/../view',
+            'book' => __DIR__ . '/../view',
         ],
     ],
 );
+
+
+
+
+///*
+//
+//    'controllers'=>[
+//        'invokables'=>[
+//            Controller\BookController::class => InvokableFactory::class,
+//        ],
+//    ],
+//    'router'=>array(
+//        'routes'=>array(
+//            'book'=>array(
+//                'type'=>'segment',
+//                'options'=>array(
+//                    'route'=>'/book[/][:action][/:id]',
+//                    'constraints'=>array(
+//                        'action'=>'[a-zA-Z][a-zA-Z0-9_-]*',
+//                        'id'=>'[0-9]+',
+//                    ),
+//                    'defaults'=>array(
+//                        'controller'=>Controller\BookController::class,
+//                        'action'=>'index',
+//                    ),
+//                ),
+//            ),
+//        ),
+//    ),
+//    'view_manager' => [
+//        'template_path_stack' => [
+//            'ZendSkeletonModule' => __DIR__ . '/../view',
+//        ],
+//    ],
+//);
+//
+//*/
---
+++ module/BookList/src/Controller/BookController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bad4iz
+ * Date: 25.01.2017
+ * Time: 22:17
+ */
+
+namespace BookList\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class BookController extends AbstractActionController{
+    public function indexAction()
+    {
+        return [];
+    }
+    public function addAction(){
+
+    }
+    public function editAction(){
+
+    }
+    public function deleteAction(){
+
+    }
+
+}
