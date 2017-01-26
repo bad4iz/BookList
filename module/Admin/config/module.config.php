98946616207b9d2114693ddc76b8719213f14532
Евгений Плотников <bad4iz@gmail.com>
2017-01-26 14:24:22 +0400

    admin Module

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -15,7 +15,7 @@ return [
                 'type'    => 'Literal',
                 'options' => [
                     // Change this to something specific to your module
-                    'route'    => '/module-specific-root',
+                    'route'    => '/module',
                     'defaults' => [
                         'controller'    => Controller\AdminController::class,
                         'action'        => 'index',
