deb7b33e1ddb018030542dd217cb515c660ce491
bad4iz <bad4iz@gmail.com>
2017-01-27 02:05:10 +0400

    book

--- module/BookList/src/BookList/Model/Book.php
+++ module/BookList/src/BookList/Model/Book.php
@@ -6,10 +6,19 @@
  * Time: 1:58
  */
 
-namespace Booklist\Model;
+namespace BookList\Model;
 
 
 class Book
 {
+public $id;
+public $title;
+public $author;
+
+public function exchangeArray($data){
+    $this->id =(!empty($data['id']))? $data['id']:null;
+    $this->title =(!empty($data['title']))? $data['title']:null;
+    $this->author =(!empty($data['author']))? $data['author']:null;
+}
 
 }
