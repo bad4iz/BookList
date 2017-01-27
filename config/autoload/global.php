ae95364f94d411916823865c05274ca190c9843f
Евгений Плотников <bad4iz@gmail.com>
2017-01-27 11:11:25 +0400

    db connect

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,16 @@
  */
 
 return [
-    // ...
+    'db' => [
+        'driver' => 'PDO',
+        'dsn' => 'mysql:dbname=books;host=127.0.0.1;port=3306',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF*\' '
+        ),
+    ],
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory,'
+        ),
+    ),
 ];
